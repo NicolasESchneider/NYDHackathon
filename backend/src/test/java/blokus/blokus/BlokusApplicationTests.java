8f42842ad8148e438d20b9194e2f520b9d87b7de
NicolasESchneider <nicoefschneider@gmail.com>
2021-01-01 11:40:23 -0500

    backend init

---
+++ backend/src/test/java/blokus/blokus/BlokusApplicationTests.java
@@ -0,0 +1,13 @@
+package blokus.blokus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlokusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
